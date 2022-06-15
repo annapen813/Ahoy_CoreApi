b47a5c9517af004d6df8f367723a399278be3f53
ananda <ananda.ethirajan@gmail.com>
2022-06-15 08:30:56 +0400

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Ahoy_Data.Data;
+using Ahoy_Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ahoy_CoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AhoyContext _context;
+
+        public UserController(AhoyContext context)
+        {
+            _context = context; 
+        }
+
+        [HttpGet]
+        public IEnumerable<UserInfo> GetUsers()
+        {
+            return _context.Users.ToList();
+        }
+
+        [HttpPost]
+        public 
+    }
+}
